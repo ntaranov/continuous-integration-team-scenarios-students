a5faefbea2de4c8b0b82773fd5d2b3ea6cbdf40e
Victorian Ionescu <victorian.ionescu@tulcea.vard.com>
2021-03-01 14:35:11 +0200

    Test w/o code

--- ci.md
+++ ci.md
@@ -3,3 +3,6 @@
   when code is deployed straight from feature branches. This list is just an interpretation  
   that I use in my [DevOps courses](http://redpill.solutions).  
   The official tutorial is [here](https://guides.github.com/introduction/flow/).
+  5. Merge/rebase commits from master. Make tests pass on the merge result.  
+  6. Deploy from the feature branch with a sneaky bug to production.
+  7. If everything is good in production for some period of time, merge changes to master. 
--- ci.test.js
+++ ci.test.js
@@ -12,6 +12,18 @@ describe('CI sequence', () => {
   });
 
   // TODO add the tests between these comments =>
+  
+  it('5. Merge/rebase commits from master. Make tests pass on the merge result.', () => {
+    expect(/.*merge.*commits.*tests\s+pass.*/ig.test(fileContents)).toBe(true);
+  });
+
+  it('6. Deploy from the feature branch to production.', () => {
+    expect(/.*Deploy.*to\s+production.*/ig.test(fileContents)).toBe(true);
+  });
+
+  it('7. If everything is good in production for some period of time, merge changes to master.', () => {
+    expect(/.*merge.*to\s+master.*/ig.test(fileContents)).toBe(true);
+  });
 
   // TODO <= add the tests between these comments
 
